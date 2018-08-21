9466666cb1f7075399456b3f4049cb3d99957849
duanzhiwei <89340545@qq.com>
2018-08-21 08:09:32 +0800

    qq

--- Modules/Admin/Resources/views/goodsimport.blade.php
+++ Modules/Admin/Resources/views/goodsimport.blade.php
@@ -13,7 +13,7 @@
          data-icon="cloud-upload"></div>
     <input type="hidden" name="doc.pic" value="" id="doc_pic">
 </div>
-<a href="demo1.xls">下载模板</a>
+&nbsp;&nbsp;<a href="demo1.xls">下载模板</a>
 
 <div style="margin: 10px;">
     <div id="goods_detail_up" data-toggle="upload" data-uploader="api/goods/import/extra"
@@ -25,5 +25,5 @@
          data-icon="cloud-upload"></div>
     <input type="hidden" name="doc.pic" value="" id="doc_pic">
 </div>
-<a href="demo2.xls">下载模板</a>
+&nbsp;&nbsp;<a href="demo2.xls">下载模板</a>
 
