0ee2e0ac054d57f7ae4c2a1484bb18d9fc66a0a3
duanzhiwei <89340545@qq.com>
2018-08-27 11:38:49 +0800

    df

--- Modules/Api/Http/routes.php
+++ Modules/Api/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
+Route::group(['domain' => 'api'.config('app.base_url')],function() {
 
-Route::group(['middleware' => 'web', 'prefix' => 'api', 'namespace' => 'Modules\Api\Http\Controllers'], function()
-{
-    Route::get('/', 'ApiController@index');
+    Route::group(['middleware' => 'web', 'prefix' => 'api', 'namespace' => 'Modules\Api\Http\Controllers'], function () {
+        Route::get('/', 'ApiController@index');
+    });
+
 });
