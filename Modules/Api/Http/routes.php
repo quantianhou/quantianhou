1e57dfb214fd1e1f5dda224347359803de76a1f0
fanhailong <121838942@qq.com>
2018-08-27 13:11:13 +0800

    通过商家编码来给出appid和appsecrt

--- Modules/Api/Http/routes.php
+++ Modules/Api/Http/routes.php
@@ -8,7 +8,7 @@ Route::group(['domain' => 'api'.config('app.base_url')],function() {
         });
 
         //商家管理
-        Route::group(['prefix' => 'merchantAccount', 'namespace' => 'merchantAccount'], function () {
+        Route::group(['prefix' => 'merchantAccount', 'namespace' => 'MerchantAccount'], function () {
             Route::any('getAppInfo', 'MerchantAccountController@getAppInfo');//通过商家编码来给出appid和appsecrt
         });
 
