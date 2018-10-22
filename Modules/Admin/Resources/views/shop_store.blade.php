5c6d9b53fc3bace35cd26b4d8e8ef03867c47a9d
Fanhailong <121838942@qq.com>
2018-10-22 23:44:33 +0800

    添加门店：部分字段修改为不必填

--- Modules/Admin/Resources/views/shop_store.blade.php
+++ Modules/Admin/Resources/views/shop_store.blade.php
@@ -290,7 +290,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>营业执照：</label>
+                        <label class="label-control">营业执照：</label>
                     </td>
                     <td>
                         许可证号： <input type="text" placeholder="请填写名称" name="business_license_num" size="30" data-rule="required"><br>
@@ -303,7 +303,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>药品经营许可证：</label>
+                        <label class="label-control">药品经营许可证：</label>
                     </td>
                     <td>
                         许可证号： <input type="text" placeholder="请填写名称" name="drug_license_num" size="30" data-rule="required"><br>
@@ -317,7 +317,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>法人身份证：</label>
+                        <label class="label-control">法人身份证：</label>
                     </td>
                     <td>
                         姓名： <input type="text" placeholder="请填写名称" name="legal_person_name" size="30" data-rule="required"><br>
@@ -331,7 +331,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>GPS证书：</label>
+                        <label class="label-control">GPS证书：</label>
                     </td>
                     <td>
                         许可证号： <input type="text" placeholder="请填写名称" name="GSP_num" size="30" data-rule="required"><br>
@@ -344,7 +344,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>食物流通许可证书：</label>
+                        <label class="label-control">食物流通许可证书：</label>
                     </td>
                     <td>
                         许可证号： <input type="text"  placeholder="请填写名称" name="food_licence_num" size="30" data-rule="required"><br>
@@ -357,7 +357,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>医疗机构许可证：</label>
+                        <label class="label-control">医疗机构许可证：</label>
                     </td>
                     <td>
                         许可证号： <input type="text" placeholder="请填写名称" name="medical_institution_num" size="30" data-rule="required"><br>
@@ -371,7 +371,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>医疗器械许可证：</label>
+                        <label class="label-control">医疗器械许可证：</label>
                     </td>
                     <td>
                         许可证号： <input type="text" placeholder="请填写名称" name="medical_app_num" size="30" data-rule="required"><br>
@@ -385,7 +385,7 @@
 
                 <tr>
                     <td align="right" width="300px;">
-                        <label class="label-control"><span style="color:red">*</span>互联网药品交易服务许可证：</label>
+                        <label class="label-control">互联网药品交易服务许可证：</label>
                     </td>
                     <td>
                         许可证号： <input type="text" placeholder="请填写名称" name="internet_med_tran_num" size="30" data-rule="required"><br>
