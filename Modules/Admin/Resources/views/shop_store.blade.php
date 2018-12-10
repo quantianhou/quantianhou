e7d71996ed59cddd959a5bad1c1643e01bcf0681
Fanhailong <121838942@qq.com>
2018-12-11 02:53:52 +0800

    Update shop_store.blade.php

--- Modules/Admin/Resources/views/shop_store.blade.php
+++ Modules/Admin/Resources/views/shop_store.blade.php
@@ -105,6 +105,7 @@
                             </select>
 
                         </div>
+                        <br/>
                         <input type="text" placeholder="请填写详细地址" name="address" size="30" data-rule="required"><span></span>
                         <select id="sel-street" class="select form-control" style="width:130px;display:none;">
                             <option value="" selected="true">请选择</option>
